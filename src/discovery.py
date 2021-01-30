a89c2fa95d65086548a0b6bb4cbfac511310fc64
Ryan Gibb <ryangibb321@gmail.com>
2021-01-30 12:52:47 +0000

    Discovery protocol TTL

--- src/discovery.py
+++ src/discovery.py
@@ -1,4 +1,5 @@
 import struct
+import time
 
 import util
 from util import NetworkException
@@ -16,25 +17,45 @@ nid_to_hst = {}
 
 def getaddrinfo(addr):
     hst, port = addr
-    nid = hst_to_nid.get(hst)
-    if nid == None:
-        raise NetworkException("No mapping for hostname '%s'" % hst)
+    entry = hst_to_nid.get(hst)
+    if entry != None:
+        nid, timestamp = entry
+        # If mapping expired
+        if time.time() - timestamp > ttl:
+            hst_to_nid[hst] = None
+            entry = None
+    if entry == None:
+        raise NetworkException("No addr mapping for hostname '%s'" % hst)
     return nid, port
 
 
 def gethostbyaddr(addr):
     nid, port = addr
-    hst = nid_to_hst.get(nid)
-    if hst == None:
-        raise NetworkException("No mapping for nid '%s'" % nid)
+    entry = nid_to_hst.get(nid)
+    if entry != None:
+        hst, timestamp = entry
+        # If mapping expired
+        if time.time() - timestamp > ttl:
+            nid_to_hst[nid] = None
+            entry = None
+    if entry == None:
+        raise NetworkException("No addr mapping for nid '%s'" % nid)
     return hst, port
 
 
 def get_locs(nid):
     locs = nid_to_locs.get(nid)
+    if locs != None:
+        valid_locs = [(loc, timestamp) for loc, timestamp in locs if time.time() - timestamp < ttl]
+        if len(valid_locs) == 0:
+            nid_to_locs[nid] = None
+            valid_locs = None
+        elif len(locs) > len(valid_locs):
+            nid_to_locs[nid] = valid_locs
+        locs = valid_locs
     if locs == None:
-        raise NetworkException("No mapping for nid '%s'" % nid)
-    return list(locs)
+        raise NetworkException("No locators mapping for nid '%s'" % nid)
+    return [loc for loc, _ in locs]
 
 
 def get_solititation(loc):
@@ -62,6 +83,8 @@ def get_advertisement(loc):
 
 
 def process_message(message, recieved_interface):
+    timestamp = time.time()
+
     # 17 bytes of struct
     message_struct = message[:17]
     (
@@ -73,14 +96,29 @@ def process_message(message, recieved_interface):
     loc = util.bytes_to_hex(loc_bytes)
     hst = message[17:].decode('utf-8')
     
-    hst_to_nid[hst] = nid
-    nid_to_hst[nid] = hst
+    hst_to_nid[hst] = nid, timestamp
+    nid_to_hst[nid] = hst, timestamp
     # Can have multiple locators for one nid
-    nid_to_locs.setdefault(nid, set()).add(loc)
+    locs = nid_to_locs.get(nid)
+    if locs == None:
+        nid_to_locs[nid] = [(loc, timestamp)]
+    else:
+        loc_already_mapped = False
+        for i in range(len(locs)):
+            mapped_loc, _ = locs[i]
+            if mapped_loc == loc:
+                loc_already_mapped = True
+                # update timestamp
+                locs[i] = loc, timestamp
+                break
+        if not loc_already_mapped:
+            locs.append((loc, timestamp))
+        nid_to_locs[nid] = locs
 
     if log_file != None:
-        util.write_log(log_file, "\n\t%s\n\t%s" % (
+        util.write_log(log_file, "\n\t%s\n\t%s\n\t%s" % (
             "%s => %s" % (hst, hst_to_nid[hst]),
+            "%s => %s" % (nid, nid_to_hst[nid]),
             "%s => %s" % (nid, nid_to_locs[nid])
         ))
 
@@ -108,11 +146,13 @@ def startup(local_nid_param, locs_joined):
     local_hst = config_section["hostname"]
 
     # Time between sending solititations in seconds
-    global wait_time
+    global wait_time, ttl
     if "wait_time" in config_section:
         wait_time = config_section["wait_time"]
     else:
         wait_time = 10
+    # discovery TTL is 3 times the wait time
+    ttl = 3 * wait_time
     
     global local_nid
     local_nid = local_nid_param
