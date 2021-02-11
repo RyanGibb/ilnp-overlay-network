3edead974342a71c74fb50fe3cbaf793f569e7cc
Ryan Gibb <ryangibb321@gmail.com>
2021-02-11 19:59:44 +0000

    experiment

--- src/experiment.py
+++ src/experiment.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import time
 from datetime import datetime
 
@@ -9,8 +11,9 @@ HOSTNAME        = util.config["discovery"]["hostname"]
 PORT            = util.config["application"].getint("port")
 REMOTE_HOSTNAME = util.config["application"]["remote_hostname"]
 REMOTE_PORT     = util.config["application"].getint("remote_port")
+RUN_TIME        = util.config["application"].getint("run_time")
 
-def heartbeat():
+def experiment():
     sock = transport.Socket()
     sock.bind(PORT)
     
@@ -19,28 +22,85 @@ def heartbeat():
     else:
         remote = None
     
-    f = open("/dev/urandom", "rb")
+    config_section = util.config["application"]
+    print(datetime.now(), " Started")
+    for k in config_section:
+        print(datetime.now(), "\t%s = %s" % (k, config_section[k]))
 
+    f = open("/dev/urandom", "rb")
+    
+    total_bytes = 0
+
+    start = None
+
+    i = 1
+    sequence_number = None
     while True:
         try:
             if remote != None:
                 remote_addr = discovery.getaddrinfo(remote)
-                data=f.read(16*1024)
-                print("%-30s <- %s" % ("%s:%d" % remote, len(data)))
+                # MTU - header size = 1440 - 44 = 1396
+                # 1396 - 8 = 1388
+                data=util.int_to_bytes(i, 8) + f.read(1388)
+                print("%s %-30s <- %d %d" % (
+                    datetime.now(),
+                    "%s:%d" % remote,
+                    len(data),
+                    i
+                ))
                 sock.send(remote_addr, data)
+                i+=1
+                if start == None:
+                    start=time.time()
+                else:
+                    wait_time = 0.1 # 100ms
+                    sleep_time = (i * wait_time) - (time.time() - start)
+                    if sleep_time > 0:
+                        time.sleep(sleep_time)
+                    else:
+                        print("WARN %f slow" % -sleep_time, file=sys.stderr)
+                total_bytes+=len(data)
         except transport.NetworkException as e:
-            print("Network Exception: %s" % e.message)
+            print("Network Exception: %s" % e.message, file=sys.stderr)
         
         while True:
             try:
                 data, src_addr = sock.receive()
-                print("%-30s -> %s" % (
+                sequence_number=util.bytes_to_int(data[:8])
+                if start == None:
+                    start=time.time()
+                print("%s %-30s -> %d %d" % (
+                        datetime.now(),
                         "%s:%d" % discovery.gethostbyaddr(src_addr),
-                        len(data)
+                        len(data),
+                        sequence_number
                 ))
+                total_bytes+=len(data)
             except transport.NetworkException as e:
+                # Allow context switching
+                time.sleep(0)
                 break
-        time.sleep(0.1)
+
+        now = time.time()
+
+        if sequence_number == 0:
+            break
+
+        if RUN_TIME != -1 and start != None and now - start > RUN_TIME:
+            if remote != None:
+                remote_addr = discovery.getaddrinfo(remote)
+                data=util.int_to_bytes(0, 8)
+                sock.send(remote_addr, data)
+                print("%s %-30s <- %d %d" % (
+                    datetime.now(),
+                    "%s:%d" % remote,
+                    len(data),
+                    0
+                ))
+            break
+    print("Total bytes: %d" % total_bytes)
+    print("Bytes/sec: %d" % (total_bytes / (now - start)))
+
 
 if __name__ == "__main__":
-    heartbeat()
+    experiment()
