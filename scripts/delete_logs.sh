4ef5647a4443368b0bb4ec0be6ce0da29112a82b
Ryan Gibb <ryangibb321@gmail.com>
2021-03-18 21:24:06 +0000

    config updates

--- scripts/delete_logs.sh
+++ scripts/delete_logs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-method=${1:-"eth"}
+method=${1:-"wifi"}
 
 echo "Deleting logs..."
-for host in alice bob; do
+for host in alice bob clare; do
 	ssh "$host"-"$method" "rm ~/ilnp-overlay-network/logs/*.log" &\
     echo "$host"
 done
